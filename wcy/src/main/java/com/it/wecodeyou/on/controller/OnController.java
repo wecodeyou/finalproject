807f100832d30b11a36843da0b4ecb8c0f6fdd8e
KCW <sduks59@gmail.com>
2020-05-05 03:45:28 +0900

    video playlist 미완

---
+++ wcy/src/main/java/com/it/wecodeyou/on/controller/OnController.java
@@ -0,0 +1,19 @@
+package com.it.wecodeyou.on.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/on")
+public class OnController {
+
+	@GetMapping("/")
+	public String sample(Model model) {
+		
+		
+		return "/on/sample";
+	}
+
+}
