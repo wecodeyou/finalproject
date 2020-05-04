62cf3fe476b5dee55b5b95c638d66f9a5a88fb7c
daesung <daesung@DESKTOP-RQED9HM>
2020-05-04 16:17:43 +0900

    mypage con/jsp 변경

--- wcy/src/main/java/com/it/wecodeyou/mypage/MypageController.java
+++ wcy/src/main/java/com/it/wecodeyou/mypage/MypageController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-// 이게 왜 안쓰이는 거지 
+// 이게 왜 안쓰이는 거지 ?
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
